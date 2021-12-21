4a38a0e8f6aaa303aeefa56661e8867ca20ad9f2
thaontp1999 <thaocoi06101999@gmail.com>
2021-12-21 11:23:42 +0700

    SQLSERVER Connection

---
+++ ConnectSQLServer/ConnectSQLServer/DBSQLServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace connectsqlserver
+
+{
+    public  class Connection
+    {   //tạo kết nối
+        SqlConnection connect = new SqlConnection("Data Source=DESKTOP-HSAI5KE;Initial Catalog=PHONE;Integrated Security=True");
+        public void Form_Load(object sender, EventArgs e)
+        {
+            connect.Open(); //mở kết nối
+            string sql = "SELECT TenDT, GiaKM, SoLuongTonKho, HeDieuHanh From Dien_Thoai FUll OUter JOIN Thuoc_TinhDT ON Dien_Thoai.idDT = Thuoc_TinhDT.idDT "; //câu lệnh SQl
+            SqlCommand cmd = new SqlCommand(sql, connect); //thực hiện lệnh truy vấn
+            cmd.CommandType = CommandType.Text; 
+            SqlDataAdapter da = new SqlDataAdapter(cmd);//nơi để luu dữ liệu lấy đc
+            DataTable dt = new DataTable(); //tạo 1 kho dữ liệu ảo
+            da.Fill(dt); //đổ dữ liệu vào kho
+            connect.Close();
+            
+        }
+    }
+    
+        
+}
---
+++ ConnectSQLServer/ConnectSQLServer/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+
+namespace ConnectSQLServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection ...");
+
+            var datasource = @"DESKTOP-HSAI5KE";
+            var database = "PHONE"; 
+            var username = "sa"; 
+            var password = "123456";
+
+            
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                //open connection
+                
+                conn.Open();
+
+                QueryEmployee(conn);
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
+
+            
+            }
+        public static void QueryEmployee(SqlConnection conn)
+        {
+            string sql = "Select idDT, TenDT, Gia, ThoiDiemNhap, SoLanXem from Dien_Thoai";
+
+            // Tạo một đối tượng Command.
+            SqlCommand cmd = new SqlCommand();
+
+            // Liên hợp Command với Connection.
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        // Chỉ số của cột Emp_ID trong câu lệnh SQL.
+                        int idDTIndex = reader.GetOrdinal("idDT"); // 0
+
+
+                        long idDT = Convert.ToInt64(reader.GetValue(0));
+                        string TenDT = reader.GetString(1);
+                        double Gia = reader.GetDouble(2);
+                        DateTime ThoiDiemNhap = reader.GetDateTime(3);
+                        int SoLanXem = reader.GetInt32(4);
+
+                        // Chỉ số của cột Mng_Id trong câu lệnh SQL.
+                        //int mngIdIndex = reader.GetOrdinal("Mng_Id");
+                        //long? mngId = null;
+
+
+                        //if (!reader.IsDBNull(mngIdIndex))
+                        //{
+                        //    mngId = Convert.ToInt64(reader.GetValue(mngIdIndex));
+                        //}
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("idDT:" + idDT);
+                        Console.WriteLine("TenDT:" + TenDT);
+                        Console.WriteLine("Gia:" + Gia);
+                        Console.WriteLine("ThoiDienNhap:" + ThoiDiemNhap);
+                        Console.WriteLine("SoLanXem:" + SoLanXem );
+                        Console.WriteLine();
+
+                    }
+                }
+            }
+        }
+    }
+}
